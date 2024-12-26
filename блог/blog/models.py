a679d4a58ffee881a273474b68f8f66095d1b1de
gabelkova123 <135582627+gabelkova123@users.noreply.github.com>
2024-12-26 15:43:42 +0700

    Update models.py

--- блог/blog/models.py
+++ блог/blog/models.py
@@ -1,34 +1,26 @@
+
 from django.db import models
-
-
-class Article(models.Model):
-    title = models.CharField(
-        max_length=255,
-    )
-    text = models.CharField(
-        max_length=1000,
-    )
-
-
-    description = models.TextField(
-        null=True,
-        blank=True,
-    )
-    category = models.ForeignKey(
-        'Category',
-        on_delete=models.SET_NULL,
-        null=True,
-    )
-
+ 
+# Create your models here.
+ 
+class Post(models.Model):
+    title = models.CharField(max_length=120)
+    description = models.TextField(default='Описание')
+    keywords = models.CharField(max_length=120, default='Ключевые слова')
+    image = models.FileField(null=True, blank=True)
+    content = models.TextField()
+    visible = models.BooleanField(default=1)
+    updated = models.DateTimeField(auto_now=True, auto_now_add=False)
+    timestamp = models.DateTimeField(auto_now=False, auto_now_add=True)
+ 
+    def __unicode__(self):
+        return self.title
+ 
     def __str__(self):
-        return 'Статья: "' + self.title + '"'
-
-     
-
-class Category(models.Model):
-    name = models.CharField(
-        max_length=255,
-    )
-
-    def __str__(self):
-        return self.name
+        return self.title
+ 
+    def get_absolute_url(self):
+        return "/%s/" %(self.id)
+ 
+    class Meta:
+        ordering = ["-id", "-timestamp"]
