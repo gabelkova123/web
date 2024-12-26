c881d01e07b332cbc96b491749b039caa82a0a73
gabelkova123 <135582627+gabelkova123@users.noreply.github.com>
2024-12-26 15:45:19 +0700

    Update admin.py

--- блог/blog/admin.py
+++ блог/blog/admin.py
@@ -1,8 +1,17 @@
+
 from django.contrib import admin
-
-from blog.models import Article, Category
-
-
-admin.site.register(Article)
-
-admin.site.register(Category)
+ 
+# Register your models here.
+ 
+from .models import Post
+ 
+class PostModelAdmin(admin.ModelAdmin):
+    list_display = ["id" ,"title", "updated", "timestamp"]
+    list_display_links = ["id", "updated"]
+    list_editable = ["title"]
+    list_filter = ["updated", "timestamp"]
+    search_fields = ["title", "content"]
+    class Meta:
+        model = Post
+ 
+admin.site.register(Post, PostModelAdmin)
