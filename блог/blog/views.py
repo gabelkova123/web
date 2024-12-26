8055c6c521e2d7e6bb87de4faeac1490fd1e134f
gabelkova123 <135582627+gabelkova123@users.noreply.github.com>
2024-12-26 15:49:26 +0700

    Update views.py

--- блог/blog/views.py
+++ блог/blog/views.py
@@ -1,12 +1,23 @@
-from django.contrib import admin
-from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
  
-urlpatterns = [
-    path('',  include('blog.urls')),
-    path('admin/', admin.site.urls),
-]
+from  blog.models import Post
  
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
  
+def home(request):
+    postList = Post.objects.filter(visible='1')
+    paginator = Paginator(postList, 4)
+    page = request.GET.get('page')
+    querysetGoods = paginator.get_page(page)
+ 
+    context = {
+        "postList": postList,
+        "title": "Главная страница блога",
+        "desc": "Описание для главной страницы",
+        "key": "ключевые, слова",
+    }
+    return render(request, "partial/home.html", context)
+ 
+def single(request, id=None):
+    return render(request, "partial/single.html")
